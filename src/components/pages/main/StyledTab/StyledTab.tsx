e4680d8aa656d1b56e69978630790b7585bd46ff
Alice2410 <devo4ka2410@gmail.com>
2023-03-12 18:18:22 +0300

    feat: add styled component for custom tab

---
+++ src/components/pages/main/StyledTab/StyledTab.tsx
@@ -0,0 +1,17 @@
+import { styled } from '@mui/material/styles';
+import { Tab } from '@mui/material';
+
+const StyledTab = styled(Tab)`
+  font-family: "Raleway","Arial", sans-serif;
+  font-size: 18px;
+  line-height: 21px;
+  font-weight: 800;
+  color: black;
+  text-transform: uppercase;
+  letter-spacing: 0.2em;
+  padding: 10px 20px 0 10px;
+  margin: 0 auto;
+  margin-bottom: 10px;
+`;
+
+export default StyledTab;
